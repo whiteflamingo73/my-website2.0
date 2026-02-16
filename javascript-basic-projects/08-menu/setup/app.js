b8a69e2a75aa17681cb35aa0a842a8c53cbdef3a
whiteflamingo73 <156980239+whiteflamingo73@users.noreply.github.com>
2026-02-16 10:25:10 -0500

    menu almost done

--- javascript-basic-projects/08-menu/setup/app.js
+++ javascript-basic-projects/08-menu/setup/app.js
@@ -71,4 +71,60 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 16.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+//load items
+window.addEventListener('DOMContentLoaded', function(){
+  //console.log('shake and bake');
+  displayMenuItems(menu);
+});
+//filter items
+filterBtns.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function(menuItem){
+      if(menuItem.category === category){
+      return menuItem;
+    }
+      
+    });
+    //console.log(menuCategory);
+    if(category === 'all'){
+      displayMenuItems(menu);
+    }
+    else{
+      displayMenuItems(menuCategory);
+    }
+  });
+});
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+
+    return `<!-- single item -->
+      <article class="menu-item">
+        <img src=${item.img} class="photo" alt=${item.title} />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">$${item.price}</h4>
+          </header>
+          <p class="item-text">${item.desc}</p>
+        </div>
+      </article>
+      <!-- end single item -->`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
