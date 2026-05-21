8fe0879cd952675e5149dc5b6a76c6a82cc6868b
whiteflamingo73 <156980239+whiteflamingo73@users.noreply.github.com>
2026-05-21 10:25:39 -0400

    car and bike progress

---
+++ FinalProject/overseer.js
@@ -0,0 +1,29 @@
+/* JS file for all of the website!
+   Even if ts is inneficient, it'll be funny
+   and I wanna see how many lines of code is written
+   and also, there may be some shared functions
+   so it may be useful, idk */
+
+
+//Functions for Homepage
+
+/* Buttons */
+
+const driveStart = document.getElementById('car');
+const trainStart = document.getElementById('train');
+const bikeStart = document.getElementById('bike');
+
+driveStart.addEventListener('click', function(){
+    window.location.href = "drive/driveHome.html";
+});
+
+trainStart.addEventListener('click', function(){
+    window.location.href = "train/trainHome.html";
+});
+
+bikeStart.addEventListener('click', function(){
+    window.location.href = "bike/bikeHome.html";
+});
+
+
+//End of Homepage
